b48859a7861b89508ee17421403f7a1e9540c8a7
rui-uiclap <rui@uiclap.com>
2022-04-18 13:35:54 -0300

    inicial

---
+++ src/components/Botao.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+    icon: String,
+    label: String
+})
+</script>
+<template>
+    <button class="button">
+        <span v-if="props.icon?.length>0" class="icon"><i class="mdi" :class="props.icon"></i></span>
+        <span v-if="props.label.length>0">{{props.label}}</span>
+    </button>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import Botao from '@/components/Botao.vue';
+import { ref } from 'vue';
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+const mostraModal = ref(false);
+</script>
+
+<template>
+    Sobre:
+    <Botao label="Info" class="is-info" @click="mostraModal=true"></Botao>
+
+    <div class="modal" :class="{'is-active' : mostraModal}">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="message is-info">
+                <div class="message-body">
+            Algumas informações sobre o app??
+
+                </div>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="mostraModal=false"></button>
+    </div>
+
+</template>
