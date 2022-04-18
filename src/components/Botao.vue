0bf02b13e07cc2a92c53137c8c251e15ffe7f107
rui-uiclap <rui@uiclap.com>
2022-04-18 13:37:52 -0300

    correção do botao

--- src/components/Botao.vue
+++ src/components/Botao.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 </script>
 <template>
     <button class="button">
-        <span v-if="props.icon?.length>0" class="icon"><i class="mdi" :class="props.icon"></i></span>
-        <span v-if="props.label.length>0">{{props.label}}</span>
+        <span v-if="(props.icon?.length ?? 0)>0" class="icon"><i class="mdi" :class="props.icon"></i></span>
+        <span v-if="(props.label?.length ?? 0)>0">{{props.label}}</span>
     </button>
 </template>
